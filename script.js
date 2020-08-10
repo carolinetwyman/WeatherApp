c05debf573c395efd65e18021f0a3a80834a5de9
Caroline Twyman <carolinetwyman@gmail.com>
2020-08-10 18:55:46 -0500

    current forecast done

--- script.js
+++ script.js
@@ -11,13 +11,38 @@ $("#myCity").on("click", function (event) {
     var citySearch = $("#myCitySearch").val();
 
     // Here we construct our URL
+    var fiveDayForecast ="https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&appid=" + mykey;
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&APPID=" + mykey;
+    var cityQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch + "&APPID=" + mykey;
+    
+    $.ajax({
+        url: fiveDayForecast,
+        method: "GET"
+    })
 
+        .then(function (response) {
 
+            for (let i = 0; i < response.list.length; i += 8) {
+                console.log(response.list[i])
+                //var fiveDayDay = $("<p>").text(response.list[i]);
+                // $(".fiveDay").text("UV Index: " + fiveDayDay);
+                // console.log("UV Index: " + response.value);
+                // //fiveDayDiv.append(fiveDayDay);
+                // $("#fiveday").append(fiveDayDiv);
+
+            }
+
+            // Log the queryURL
+            console.log(fiveDayForecast);
+
+            // Log the resulting object
+            console.log(response);
+        
+        })
     $.ajax({
         url: queryURL,
         method: "GET"
-    });
+    })
 
 
         //$("#myWeatherResult").text(JSON.stringify(response));
@@ -34,6 +59,9 @@ $("#myCity").on("click", function (event) {
             $(".city").html("<h1>" + response.name + " Weather Details</h1>");
             $(".wind").text("Wind Speed: " + response.wind.speed);
             $(".humidity").text("Humidity: " + response.main.humidity);
+            var icon = response.weather[0].icon;
+            console.log(icon)
+            $(".weather").attr("src","http://openweathermap.org/img/wn/" + icon + "@2x.png");
 
             // Convert the temp to fahrenheit
             var tempF = (response.main.temp - 273.15) * 1.80 + 32;
@@ -46,5 +74,56 @@ $("#myCity").on("click", function (event) {
             console.log("Wind Speed: " + response.wind.speed);
             console.log("Humidity: " + response.main.humidity);
             console.log("Temperature (F): " + tempF);
-        });
+        })
+        $.ajax({
+            url: cityQueryURL,
+            method: "GET"
+        })
+    
+            .then(function (response) {
+    
+                // Log the queryURL
+                console.log(cityQueryURL);
+    
+                // Log the resulting object
+                console.log(response);
+    
+                // Transfer content to HTML
+                // $(".lat").text("lat: " + response.coord.lat);
+                var lat = response.city.coord.lat
+                var lon = response.city.coord.lon
+                //return (lat)
+    
+                console.log("lat: " + response.city.coord.lat);
+                console.log("lon: " + response.city.coord.lon);
+                var queryURLUV = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&APPID=" + mykey;
+    $.ajax({
+        url: queryURLUV,
+        method: "GET"
     })
+
+        .then(function (response) {
+
+            // Log the queryURL
+            console.log(queryURLUV);
+
+            // Log the resulting object
+            console.log(response);
+
+            // Transfer content to HTML
+            var date = response.date_iso
+            $(".date").text("The Weather for today, " + date);
+            var uv = response.value
+            $(".uvIndex").text("UV Index: " + uv);
+            console.log("UV Index: " + uv);
+            if (uv < 4) {
+                $(".uvIndex").addClass("favorable")
+            } else if (8 > uv > 4) {
+                $(".uvIndex").addClass("moderate")
+            } else if (uv > 8) {
+                $(".uvIndex").addClass("severe")
+            }
+        })
+            })
+
+});
